6bd5491320f1f5c5f6b7b8aee8c101747223758c
JesseT-utu <tahvo.tornberg@gmail.com>
2026-04-20 01:24:07 +0300

    Vercel json error fix

--- server.ts
+++ server.ts
@@ -113,9 +113,10 @@ async function startServer() {
     });
   }
 
+  if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running at http://0.0.0.0:${PORT}`);
+    console.log(`Server running locally at http://localhost:${PORT}`);
   });
+  }
 }
-
 startServer();
